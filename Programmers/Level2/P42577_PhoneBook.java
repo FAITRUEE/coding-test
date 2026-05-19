e60fafdb007d1c24a56147f349d4cd2b39b2e875
SEONGJIN <165881199+FAITRUEE@users.noreply.github.com>
2026-05-19 15:11:46 +0900

    [Programmers/Lv.2] P42577- 전화번호 목록

---
+++ Programmers/Level2/P42577_PhoneBook.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashSet <String> set = new HashSet<>();
+        boolean answer = true;
+        
+        for(String phone : phone_book) {
+            set.add(phone);
+        }
+        
+        for(String phone : phone_book){
+            for(int i = 1; i < phone.length(); i++){
+                if(set.contains(phone.substring(0, i))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
